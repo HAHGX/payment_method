4987cd1e30c01a9eebdd62e9432b54d9a48ddc06
aliomattux <kyle.waid@gcotech.com>
2014-12-01 14:08:54 -0600

    First Commit

---
+++ models/account_invoice.py
@@ -0,0 +1,8 @@
+from openerp.osv import osv, fields
+
+class AccountInvoice(osv.osv):
+    _inherit = 'account.invoice'
+    _columns = {
+	'sale_order': fields.many2one('sale.order', 'Sale Order', readonly=True),
+	'purchase_order': fields.many2one('purchase.order', 'Purchase Order'),
+    }
